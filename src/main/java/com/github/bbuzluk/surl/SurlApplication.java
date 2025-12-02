7aef29be039cbbc9c50eb3a632aa0892f4c62c25
Baran Buzluk <baranbuzluk@gmail.com>
2025-12-02 23:31:55 +0300

    feat(app): add SurlConfig to load config from application.yml

--- src/main/java/com/github/bbuzluk/surl/SurlApplication.java
+++ src/main/java/com/github/bbuzluk/surl/SurlApplication.java
@@ -1,12 +1,22 @@
 package com.github.bbuzluk.surl;
 
+import com.github.bbuzluk.surl.data.model.SurlConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SurlApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SurlApplication.class, args);
+  }
+
+  @Bean
+  SurlConfig surlConfig(
+      @Value("${surl.domain:http://localhost:8080}") String domain,
+      @Value("${surl.max-attempts:5}") int maxShortCodeGenerationAttempts) {
+    return new SurlConfig(domain, maxShortCodeGenerationAttempts);
   }
 }
---
+++ src/main/java/com/github/bbuzluk/surl/data/model/SurlConfig.java
@@ -0,0 +1,3 @@
+package com.github.bbuzluk.surl.data.model;
+
+public record SurlConfig(String domain, int maxShortCodeGenerationAttempts) {}
