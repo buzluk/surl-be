c3e9ed0991da7dd3f8b7a70641c6c1bf572d9db0
Baran Buzluk <baranbuzluk@gmail.com>
2026-04-08 23:32:00 +0300

    refactor(api): remove generic api response structure

--- src/main/java/com/github/buzluk/surl/core/data/dto/PageMeta.java
+++
@@ -1,7 +0,0 @@
-package com.github.buzluk.surl.core.data.dto;
-
-public record PageMeta(int page, int size, long totalElements) {
-  public int totalPages() {
-    return (int) Math.ceil((double) totalElements / size);
-  }
-}
