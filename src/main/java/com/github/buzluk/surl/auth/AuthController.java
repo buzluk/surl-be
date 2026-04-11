53d8c61d4c3f4e0d5b5e2e71b1ac03a714e59adf
Baran Buzluk <baranbuzluk@gmail.com>
2026-04-11 14:00:14 +0300

    feat(auth): relocate login request and add validation

--- src/main/java/com/github/buzluk/surl/auth/AuthController.java
+++ src/main/java/com/github/buzluk/surl/auth/AuthController.java
@@ -1,8 +1,9 @@
 package com.github.buzluk.surl.auth;
 
 import com.github.buzluk.surl.auth.data.dto.AuthToken;
+import com.github.buzluk.surl.auth.data.dto.LoginRequest;
 import com.github.buzluk.surl.auth.service.AuthService;
-import com.github.buzluk.surl.user.data.dto.LoginRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ public class AuthController {
   private final AuthService authService;
 
   @PostMapping("/login")
-  public String login(@RequestBody LoginRequest request) {
+  public String login(@Valid @RequestBody LoginRequest request) {
     AuthToken login = authService.login(request.username(), request.password());
     return login.token();
   }
--- src/main/java/com/github/buzluk/surl/user/data/dto/LoginRequest.java
+++
@@ -1,3 +0,0 @@
-package com.github.buzluk.surl.user.data.dto;
-
-public record LoginRequest(String username, String password) {}
